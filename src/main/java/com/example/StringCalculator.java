516634265e38d67c88f4c267af5bf57afa464dcc
vamsi <vamsiplaty@gmail.com>
2024-06-08 18:52:19 +0530

    Welcome message added instead of hello world

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -8,7 +8,7 @@ public class StringCalculator
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to string calculator" );
     }
 
     public int add(String numbers) {
