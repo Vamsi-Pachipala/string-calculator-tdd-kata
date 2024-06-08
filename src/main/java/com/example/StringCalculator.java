a63535958691707bb62cd2abc959fae3746ffa1d
vamsi <vamsiplaty@gmail.com>
2024-06-08 18:48:04 +0530

    Initial project setup with Maven

---
+++ src/main/java/com/example/StringCalculator.java
@@ -0,0 +1,18 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringCalculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public int add(String numbers) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+// src/test/java/com/example/StringCalculatorTest.java
+
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+    @Test
+    public void testEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+}
